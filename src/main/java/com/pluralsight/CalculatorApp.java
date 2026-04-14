b652b9000e4eb2328301149108954504eae8f470
ramenmac <kevinnguyen4200@gmail.com>
2026-04-14 09:22:06 -0500

    Complete future value method and add println method

--- src/main/java/com/pluralsight/CalculatorApp.java
+++ src/main/java/com/pluralsight/CalculatorApp.java
@@ -6,11 +6,11 @@ public class CalculatorApp {
 
         // Initialize Scanner and output banking menu
         Scanner input = new Scanner(System.in);
-        System.out.println("Welcome to the Ramen Banking App");
-        System.out.println("What calculator would you like to use?");
-        System.out.println("\t(M)ortgage Calculator");
-        System.out.println("\t(F)uture Value");
-        System.out.println("\t(P)resent Value");
+        println("Welcome to the Ramen Banking App");
+        println("What calculator would you like to use?");
+        println("\t(M)ortgage Calculator");
+        println("\t(F)uture Value");
+        println("\t(P)resent Value");
         String userChoice = input.next();
 
         // Switch cases based on which calculator user need
@@ -30,32 +30,40 @@ public class CalculatorApp {
     }
     public static void calculateLoan(Scanner input){
         // Prompt user for loan information
-        System.out.println("What is the principal loan amount?");
+        println("What is the principal loan amount?");
         double principalAmount = input.nextDouble();
-        System.out.println("What is the interest rate for the loan? (x.xxx%)");
+
+        println("What is the interest rate for the loan? (x.xxx%)");
         double interestRate = input.nextDouble() / 1200;
-        System.out.println("What is the length of the loan? (in years)");
+
+        println("What is the length of the loan? (in years)");
         int loanLength = input.nextInt() * 12;
 
         // Calculate monthly payment and total interest with formulas
         double monthPayment = (principalAmount * (interestRate * (Math.pow(1 + interestRate, loanLength)))) / ((Math.pow(1 + interestRate, loanLength))- 1);
         double totalInterest = (monthPayment * loanLength) - principalAmount;
-
-        // Output results
         System.out.printf("A $%.2f loan at %.3f%% interest for %d years would have a $%.2f/mo payment with a total interest of $%.2f", principalAmount, interestRate * 1200, loanLength / 12, monthPayment, totalInterest);
     }
     public static void calculateFuture(Scanner input){
-        System.out.println("What is the principal deposit amount?");
-        float principalAmount = input.nextFloat();
-        System.out.println("What is the interest rate for the CD? (x.xxx%");
-        float interestRate = input.nextFloat();
-        System.out.println("What is the length for the CD? (in years)");
+        // Prompt user for deposit information
+        println("What is the principal deposit amount?");
+        double principalAmount = input.nextFloat();
+
+        println("What is the interest rate for the CD? (x.xxx%");
+        double interestRate = input.nextFloat() / 100;
+
+        println("What is the length for the CD? (in years)");
         int depositLength = input.nextInt();
 
-
-
+        // Calculate and output
+        double totalValue = principalAmount * (Math.pow((1 + (interestRate / 365)), 365 * depositLength));
+        double totalInterest = totalValue - principalAmount;
+        System.out.printf("If you deposit $%.2f in a CD that earns %.2f%% interest and matures in %d year(s), your CD's ending balance will be $%.2f and you would have earned $%.2f in interest", principalAmount, interestRate * 100, depositLength, totalValue, totalInterest);
     }
     public static void calculatePresent(Scanner input){
 
+    }
+    public static void println(String message){
+        System.out.println(message);
     }
 }
